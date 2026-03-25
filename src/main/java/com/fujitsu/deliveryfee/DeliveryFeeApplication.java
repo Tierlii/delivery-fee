6484911634560f06795f929ce1adddf554523c7c
Arseni Terletski <arterl@taltech.ee>
2026-03-26 00:06:16 +0200

    chore: initial commit

---
+++ src/main/java/com/fujitsu/deliveryfee/DeliveryFeeApplication.java
@@ -0,0 +1,13 @@
+package com.fujitsu.deliveryfee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryFeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryFeeApplication.class, args);
+	}
+
+}
