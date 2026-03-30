c826b273c1469d0d2077009537ebda89864031d3
Arseni Terletski <arterl@taltech.ee>
2026-03-30 23:49:14 +0300

    fix WeatherImportServiceTest

--- src/test/java/com/fujitsu/deliveryfee/integration/WeatherImportServiceTest.java
+++ src/test/java/com/fujitsu/deliveryfee/integration/WeatherImportServiceTest.java
@@ -1,30 +1,60 @@
 package com.fujitsu.deliveryfee.integration;
 
-import org.junit.jupiter.api.Test;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.atLeast;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.fujitsu.deliveryfee.domain.enums.City;
+import com.fujitsu.deliveryfee.domain.model.ParsedWeatherObservation;
 import com.fujitsu.deliveryfee.repository.WeatherObservationRepository;
 import com.fujitsu.deliveryfee.service.WeatherImportService;
 
+@ExtendWith(MockitoExtension.class)
 class WeatherImportServiceTest {
+
+    @Mock
+    private WeatherApiClient weatherApiClient;
+
+    @Mock
+    private WeatherXmlParser weatherXmlParser;
+
+    @Mock
+    private StationToCityMapper stationToCityMapper;
+
+    @Mock
+    private WeatherObservationRepository repository;
+
+    @InjectMocks
+    private WeatherImportService service;
 
     @Test
     void shouldImportWeatherData() {
-        WeatherObservationRepository repository = mock(WeatherObservationRepository.class);
+        String xml = "<observations></observations>";
 
-        WeatherImportService service = new WeatherImportService(
-                null, // WeatherApiClient (можно замокать при желании)
-                null, // WeatherXmlParser
-                null, // Mapper
-                repository
-        );
+        ParsedWeatherObservation parsed = new ParsedWeatherObservation();
+        parsed.setStationName("Tartu-Tõravere");
+        parsed.setWmoCode("26242");
+        parsed.setAirTemperature(-2.1);
+        parsed.setWindSpeed(4.7);
+        parsed.setWeatherPhenomenon("Light snow");
+        parsed.setObservationTime(LocalDateTime.now());
 
-        // просто проверяем, что не падает
+        when(weatherApiClient.fetchWeatherData()).thenReturn(xml);
+        when(weatherXmlParser.parse(xml)).thenReturn(List.of(parsed));
+        when(stationToCityMapper.mapToCity("Tartu-Tõravere"))
+        .thenReturn(Optional.of(City.TARTU));
         service.importWeatherData();
 
-        verify(repository, atLeast(0)).save(any());
+        verify(repository, times(1)).save(ArgumentMatchers.any());
     }
 }
